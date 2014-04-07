2df7b443c10a73e0edc532427d29e3f64578ff64
Anthony SIMONET <anthony.simonet@inria.fr>
2014-04-07 13:32:29 -0500

    New script to publish metadata updates

---
+++ util/metadata_tracker.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import signal
+from threading import Thread, Event
+import json
+import pprint
+import time
+from datetime import datetime
+pp = pprint.PrettyPrinter(indent=4).pprint
+
+from subprocess import call
+
+sys.path.append("/Users/asimonet/projets/branches/python-catalog-client")
+
+from globusonline.catalog.client import dataset_client
+from globusonline.catalog.client.rest_client import RestClientError
+from globusonline.catalog.client.operators import Op
+
+dataset_url = "https://catalog-alpha.globuscs.info/service/dataset"
+catalog_id = 1
+
+ep_name = "asimonet#aps"
+ep_path = "/~/"
+
+limit = 50
+
+ad_jar = "/Users/asimonet/projets/branches/ActiveData/dist/active-data-lib-0.2.0.jar"
+aps_jar = "/Users/asimonet/projets/branches/active-data-aps/target/active-data-aps-1.0-SNAPSHOT.jar"
+
+shutdown = False
+
+def start_tracking(catalog_client):
+	last_check = datetime.utcnow()
+
+	while not shutdown:
+		time.sleep(15)
+
+		success = 0
+		failure = 0
+		selector = [("modified", Op.GT, last_check.isoformat(' '))]
+		_, data = catalog_client.get_dataset_annotations(catalog_id, selector_list=selector, annotation_list = ['id', 'modified'])
+		
+		print data
+		
+		for id in data:
+			ret = call(["java", \
+				"-cp", "%s:%s" % (ad_jar, aps_jar), \
+				"org.inria.activedata.examples.cmdline.PublishTransition", \
+				"localhost", \
+				"-m", "org.inria.activedata.aps.models.APSModel", \
+				"-t", "metadata.update", \
+				"-sid", "metadata", \
+				"-uid", str(id['id'])])
+			
+			if ret == 0:
+				success += 1
+			else:
+				failure += 1
+			
+		print "Published update transitions (success/failures): %d/%d" % (success, failure)
+		last_check = datetime.utcnow()
+	
+	sys.exit(0)
+		
+
+def sighandler(signum, frame):
+	print "sighandler!"
+	global shutdown
+	shutdown = True
+		
+# Main code
+if len(sys.argv) != 1:
+	print "Usage: metadata_tracker.py <goauth token>"
+else:
+	signal.signal(signal.SIGTERM, sighandler)
+	signal.signal(signal.SIGINT, sighandler)
+
+	catalog_client = dataset_client.DatasetClient(sys.argv[2], base_url=dataset_url)
+	start_tracking(catalog_client)
